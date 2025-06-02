ba2aaf4ad24254ff5a97645fa4189c302bce0a47
Povilas Brilius <pbrilius@gmail.com>
2025-06-02 22:56:17 +0300

    Redactionist signature...

--- config/db.php
+++ config/db.php
@@ -1,10 +1,11 @@
-<?php
+﻿<?php
 
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => '',
+    'password' =>
+'$2a$04$6zzVmEm.ADqyaytR6kdEM.Muy3mV6p.3ZpgsCZqdp3Catwuc4C02G',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
