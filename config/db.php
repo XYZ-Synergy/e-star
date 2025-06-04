bd003813226847edea6ec4bc1faa72b7f238d825
Povilas Brilius <pbrilius@gmail.com>
2025-06-04 16:58:46 +0300

    Kosmetiniai db pataisymai.

--- config/db.php
+++ config/db.php
@@ -4,8 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' =>
-'$2a$04$6zzVmEm.ADqyaytR6kdEM.Muy3mV6p.3ZpgsCZqdp3Catwuc4C02G',
+    'password' => '$2a$04$6zzVmEm.ADqyaytR6kdEM.Muy3mV6p.3ZpgsCZqdp3Catwuc4C02G',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
