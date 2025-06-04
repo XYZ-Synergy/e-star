0c482b018fa3844642e9317cf0bcd26738925333
Povilas Brilius <pbrilius@gmail.com>
2025-06-04 17:01:33 +0300

    AJAX little files...

---
+++ views/content/about.php
@@ -0,0 +1,6 @@
+﻿<div class="ajax-content">
+    <h2>Apie Mus ir "E-Star" Projektą</h2>
+    <p>"E-Star" projektas yra ambicinga iniciatyva, skirta sukurti dinamišką ir interaktyvią socialinio tinklo patirtį, prieinamą kiekvienam, nepriklausomai nuo įrenginio. Mūsų tikslas – sujungti žmones ir suteikti jiems galimybę dalintis patirtimi bei atrasti naujas galimybes.</p>
+    <p>Dirbame siekdami užtikrinti sklandų veikimą tiek pigesniuose, tiek brangesniuose mobiliuosiuose telefonuose ir "Wi-Fi" įjungtuose planšetiniuose kompiuteriuose, pasitelkiant modernias žiniatinklio technologijas.</p>
+    <p>Ačiū, kad esate su mumis!</p>
+</div>
---
+++ views/content/news-content.php
@@ -0,0 +1,10 @@
+﻿<div class="ajax-content">
+    <h2>Paskutinės Naujienos FOO BAR Priemiestyje!</h2>
+    <p>Sveiki atvykę į mūsų naujienų skiltį! Čia rasite visą aktualiausią informaciją apie įvykius ir akcijas jūsų mieste.</p>
+    <ul>
+        <li>**2025-05-28:** Atidaryta nauja "E-Star" parduotuvė ZIP salone!</li>
+        <li>**2025-05-27:** Didžiosios nuolaidos visiems išmaniesiems telefonams.</li>
+        <li>**2025-05-26:** Artimiausi renginiai bendruomenės centre.</li>
+    </ul>
+    <p>Sekite mus, kad nepraleistumėte svarbių atnaujinimų!</p>
+</div>
