d7d72db98c053c66f019f992c2876416ee23aa50
Povilas Brilius <pbrilius@gmail.com>
2025-06-09 14:42:36 +0300

    Offline mode HP!

--- controllers/AjaxController.php
+++ controllers/AjaxController.php
@@ -42,4 +42,34 @@ class AjaxController extends Controller
     }
 
     // Galite pridėti daugiau panašių veiksmų (pvz., actionGetProducts, actionGetEvents ir t.t.)
+    /**
+     * Veiksmas naujo naujienų straipsnio pridėjimui per AJAX.
+     * Grąžina JSON atsakymą.
+     *
+     * @return array
+     */
+    public function actionCreateNews()
+    {
+        \Yii::$app->response->format = Response::FORMAT_JSON; // Nustatome atsakymo formatą į JSON
+
+        $model = new News(); // Sukuriame naują News modelio instanciją
+
+        // Tikriname, ar forma buvo pateikta (POST užklausa) ir ar duomenys gali būti įkelti į modelį
+        if ($model->load(\Yii::$app->request->post())) {
+            if ($model->save()) {
+                // Jei išsaugota sėkmingai, grąžiname sėkmės pranešimą ir naujai sukurtą straipsnį
+                return ['success' => true, 'message' => 'Straipsnis sėkmingai pridėtas!', 'newsItem' => $model->toArray()];
+            } else {
+                // Jei išsaugojimo metu įvyko klaida, grąžiname klaidų pranešimus
+                return ['success' => false, 'message' => 'Nepavyko išsaugoti straipsnio.', 'errors' => $model->getErrors()];
+            }
+        }
+
+        // Jei tai GET užklausa (formos atvaizdavimui), grąžiname tik formos HTML
+        // Nustatome layout'ą į false, nes grąžiname tik formos dalį
+        $this->layout = false;
+        return $this->render('create-news-form', [
+            'model' => $model,
+        ]);
+    }
 }
