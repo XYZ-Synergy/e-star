63055f27c749eabb9e7dfd650d43a45fa679f002
Povilas Brilius <pbrilius@gmail.com>
2025-06-12 18:37:18 +0300

    Atomic Iskander - Kalingrad...

--- views/ajax/news-content.php
+++ views/ajax/news-content.php
@@ -1,13 +1,17 @@
 <div class="ajax-content">
     <h2>Paskutinės Naujienos FOO BAR Priemiestyje!</h2>
 
-    <div id="news-list-container"> <?php if (!empty($news)): ?>
+    <div id="news-list-container">
+        <?php if (!empty($news)): ?>
             <?php foreach ($news as $item): ?>
-                <div class="news-item">
-                    <h3><?= \yii\helpers\Html::encode($item->title); ?></h3>
+                <div class="news-item" id="news-item-<?= $item->id ?>"> <h3><?= \yii\helpers\Html::encode($item->title); ?></h3>
                     <small>Paskelbta: <?= \Yii::$app->formatter->asDatetime($item->created_at); ?></small>
                     <p><?= \yii\helpers\Html::encode($item->content); ?></p>
-                    <hr>
+
+                    <button class="btn btn-info btn-sm view-comments-btn" data-news-id="<?= $item->id ?>" data-url="<?= \yii\helpers\Url::to(['ajax/get-comments', 'news_id' => $item->id]); ?>">
+                        Peržiūrėti Komentarus
+                    </button>
+                    <div class="comments-area" id="comments-area-<?= $item->id ?>" style="display: none;"></div> <hr>
                 </div>
             <?php endforeach; ?>
         <?php else: ?>
