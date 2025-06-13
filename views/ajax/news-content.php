66ed63b60e97c0c8ee2610681ef4f116aabeae3d
Povilas Brilius <pbrilius@gmail.com>
2025-06-13 17:13:46 +0300

    Govt fix-up!

--- views/ajax/news-content.php
+++ views/ajax/news-content.php
@@ -8,17 +8,33 @@ use yii\widgets\LinkPager; // Naujas importas
         <?php if (!empty($news)): ?>
             <?php foreach ($news as $item): ?>
                 <div class="news-item" id="news-item-<?= $item->id ?>">
-                    <h3><?= \yii\helpers\Html::encode($item->title); ?></h3>
-                    <small>Paskelbta: <?= \Yii::$app->formatter->asDatetime($item->created_at); ?></small>
-                    <p><?= \yii\helpers\Html::encode($item->content); ?></p>
+                    <h3><?= Html::encode($item->title); ?></h3>
+                    <p><?= Html::encode($item->content); ?></p>
 
-                    <button class="btn btn-info btn-sm view-comments-btn" data-news-id="<?= $item->id ?>" data-url="<?= \yii\helpers\Url::to(['ajax/get-comments', 'news_id' => $item->id]); ?>">
-                        Peržiūrėti Komentarus
-                    </button>
+                    <?php if (!Yii::$app->user->isGuest): ?> <button class="btn btn-info btn-sm view-comments-btn" data-news-id="<?= $item->id ?>" data-url="<?= \yii\helpers\Url::to(['ajax/get-comments', 'news_id' => $item->id]); ?>">
+                            Peržiūrėti Komentarus
+                        </button>
+                        <button class="btn btn-primary btn-sm like-news-btn" data-news-id="<?= $item->id ?>" data-url="<?= \yii\helpers\Url::to(['ajax/like-news', 'id' => $item->id]); ?>">
+                            <i class="far fa-thumbs-up"></i> Patinka (<span id="likes-count-<?= $item->id ?>"><?= $item->likes_count ?></span>)
+                        </button>
 
-                    <button class="btn btn-primary btn-sm like-news-btn" data-news-id="<?= $item->id ?>" data-url="<?= \yii\helpers\Url::to(['ajax/like-news', 'id' => $item->id]); ?>">
-                        <i class="far fa-thumbs-up"></i> Patinka (<span id="likes-count-<?= $item->id ?>"><?= $item->likes_count ?></span>)
-                    </button>
+                        <?php if (Yii::$app->user->can('updateNews', ['news' => $item]) || Yii::$app->user->can('admin')): ?>
+                            <?= Html::a('Redaguoti', ['news/update', 'id' => $item->id], ['class' => 'btn btn-warning btn-sm']); ?>
+                        <?php endif; ?>
+
+                        <?php if (Yii::$app->user->can('deleteNews', ['news' => $item]) || Yii::$app->user->can('admin')): ?>
+                            <?= Html::a('Trinti', ['news/delete', 'id' => $item->id], [
+                                'class' => 'btn btn-danger btn-sm',
+                                'data' => [
+                                    'confirm' => 'Ar tikrai norite ištrinti šį straipsnį?',
+                                    'method' => 'post',
+                                ],
+                            ]); ?>
+                        <?php endif; ?>
+
+                    <?php else: ?>
+                        <p><small>Prisijunkite, kad galėtumėte komentuoti ir spausti "Patinka".</small></p>
+                    <?php endif; ?>
 
                     <div class="comments-area" id="comments-area-<?= $item->id ?>" style="display: none;"></div>
                     <hr>
