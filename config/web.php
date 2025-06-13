fe740a31994e11f990aa4501af3c61ceb4879b16
Povilas Brilius <pbrilius@gmail.com>
2025-06-13 21:15:22 +0300

    Initial attitude /.../

--- config/web.php
+++ config/web.php
@@ -45,7 +45,7 @@ $config = [
         'db' => $db,
 
         'urlManager' => [
-            'enablePrettyUrl' => false,
+            'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
                 'login' => 'site/login', // Patogus URL prisijungimui
